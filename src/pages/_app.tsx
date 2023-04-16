223c456b22691e876aed45b5be575408ec3a0e7f
Te1m0z <dmitriy.balamozhnov@gmail.com>
2023-04-17 00:11:15 +0400

    настройка scss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+
+/* STYLES */
+import '@/styles/app.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
